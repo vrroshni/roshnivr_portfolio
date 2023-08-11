6a776dfd32f2a5b0c4e1b0b4c8d8c6a6bf6e04fe
Roshni V R <vrroshni2001@gmail.com>
2023-08-11 16:11:06 +0530

    completed home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,52 @@
+
+import AnimatedTxt from '@/components/AnimatedTxt'
 import Image from 'next/image'
+import ProfilePic from '../../public/developer-pic-1.png';
+import Link from 'next/link';
+import { BsArrowRight, BsFillArrowDownCircleFill, BsLinkedin } from 'react-icons/bs';
+import { FaGithubSquare } from 'react-icons/fa';
+import AppParticles from '@/components/AppParticles';
+import HireMe from '@/components/HireMe';
+
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className=' relative flex items-center text-dark w-full h-auto md:pt-4'>
+      <AppParticles />
+      <div className="inline-block z-0 bg-light p-8 md:p-4 pt-0 w-full">
+
+        <div className='flex flex-col md:flex-row items-center justify-between w-full'>
+
+          <div className="w-full md:w-1/2">
+            <Image src={ProfilePic}
+              width={700}
+              height={700}
+              alt="Picture of the author" />
+          </div>
+          <div className="w-full mt-0 md:mt-4 md:w-1/2 flex flex-col items-center md:items-start self-center md:self-start px-4 md:px-0">
+            <AnimatedTxt text='Turning Vision into Reality with code and Design' className='!text-4xl md:!text-6xl !text-center md:!text-left' />
+            <p className='my-4 text-base md:text-lg font-medium'>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, aut tempore iste magnam sed eveniet non eum mollitia excepturi dolor! Obcaecati, facere hic impedit nihil atque placeat ducimus cumque vel.
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla, aut tempore iste magnam sed eveniet non eum mollitia excepturi dolor! Obcaecati, facere hic impedit nihil atque placeat ducimus cumque vel.
+            </p>
+            <div className='flex items-center justify-center md:items-start mt-2'>
+              <Link href={""} target='_blank' className='flex items-center bg-black text-white p-2 px-4 rounded-lg text-lg font-semibold hover:text-black hover:bg-white border border-solid border-transparent hover:border-black' download={true}>
+                Resume <BsFillArrowDownCircleFill className="w-6 ml-1" />
+              </Link>
+              <Link href="mailto:vr7roshni@gmail.com" target='_blank' className='ml-2 mt-2 text-lg font-medium capitalize text-dark underline '>Contact</Link>
+              <a className="bg-white mt-2 md:mt-0 ml-0 md:ml-3 p-3 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack " href="https://linkedin.com" target="_blank">
+                <BsLinkedin />
+              </a>
+
+              <a className="bg-white mt-2 md:mt-0 ml-0 md:ml-3 p-3 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack " href="https://github.com" target="_blank">
+                <FaGithubSquare />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <HireMe className={"bottom-4 left-4 hidden  md:block"} />
     </main>
+
   )
 }
---
+++ src/components/AnimatedTxt.tsx
@@ -0,0 +1,58 @@
+'use client'
+import React from 'react'
+import { motion } from 'framer-motion';
+
+
+const quote = {
+    initial: {
+        opacity: 1,
+    },
+    animate: {
+        opacity: 1,
+        transition: {
+            delay: 0.5,
+            staggerChildren: 0.08,
+
+        }
+    }
+}
+const singleWord = {
+    initial: {
+        opacity: 0,
+        y: 50,
+
+    },
+    animate: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            duration: 1,
+
+        }
+    }
+}
+
+interface AnimatedTxtProps {
+    text: string;
+    className?: string;
+}
+const AnimatedTxt: React.FC<AnimatedTxtProps> = ({ text, className }) => {
+    return (
+        <div className='w-full mx-auto py-2 flex items-center justify-center md:justify-start   overflow-hidden'>
+            <motion.h1
+                className={`inline-block w-full text-dark font-bold capitalize ${className}`}
+                variants={quote}
+                initial="initial"
+                animate="animate"
+            >
+                {text.split(" ").map((word, index) => (
+                    <motion.span key={index + '-word'} className='inline-block' variants={singleWord}>
+                        {word}&nbsp;
+                    </motion.span>
+                ))}
+            </motion.h1>
+        </div>
+    );
+};
+
+export default AnimatedTxt;
